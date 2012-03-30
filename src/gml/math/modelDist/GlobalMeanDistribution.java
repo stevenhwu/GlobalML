3cf814968f94ab082534aa61f1c7776bf08f2117
Steven Wu <stevenhwu@gmail.com>
2012-03-30 16:39:54 -0400

    remove old dir, no actual code change

--- src/gml/math/modelDist/GlobalMeanDistribution.java
+++
@@ -1,8 +0,0 @@
-package gml.math.modelDist;
-
-import gml.math.NormalDistribution;
-
-public class GlobalMeanDistribution extends NormalDistribution {
-//public class GlobalMeanDistribution extends CauchyDistribution {
-
-}
